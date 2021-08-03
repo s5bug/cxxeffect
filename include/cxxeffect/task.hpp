c62919decae9d5ee983359deee02748106d608d3
Alecto Irene Perez <perez.cs@pm.me>
2021-08-03 17:21:59 -0600

    Reimplemented task in terms of std::function thunk
    This eliminates the dependency on std::shared_ptr and makes tasks copyable. It should also help to simplify the memory management.

--- include/cxxeffect/task.hpp
+++ include/cxxeffect/task.hpp
@@ -1,136 +1,53 @@
 #pragma once
-
-#include <exception>
 #include <functional>
 #include <future>
-#include <memory>
-#include <variant>
 
 namespace eff {
+    template <class F, class T>
+    using flatmap_t = decltype(std::declval<F>()(std::declval<T>()).unsafeRunSync());
+    template <class F, class T>
+    using map_t = decltype(std::declval<F>()(std::declval<T>()));
+    template <class T>
+    class task {
+        std::function<T()> thunk;
+       public:
+        task() = default;
+        task(task const&) = default;
+        task(task&&) = default;
+        task(std::function<T()> thunk) : thunk(std::move(thunk)) {}
 
-    template<typename A>
-    class task;
-
-    template<typename A>
-    class raw_task {
-        public:
-        virtual A unsafeRunSync() const = 0;
-        virtual std::future<A> unsafeRunAsync() const = 0;
-    };
-
-    template<typename A>
-    class raw_task_pure final : public raw_task<A> {
-        A value;
-        public:
-        raw_task_pure(A a) : value(a) {}
-
-        A unsafeRunSync() const {
-            return value;
+        static task pure(T value) {
+            return task([=]() { return value; });
+        }
+        static auto delay(std::function<T()> thunk) {
+            return task(thunk);
         }
 
-        std::future<A> unsafeRunAsync() const {
-            return std::async(std::launch::async, [*this]() { return value; });
+        template <class F>
+        auto map(F func) const -> task<map_t<F, T>> {
+            return {
+                    [f = std::move(func), input = *this]() {
+                    return f(input.unsafeRunSync());
+                }
+            };
         }
-    };
 
-    template<typename A>
-    class raw_task_delay final : public raw_task<A> {
-        std::function<A ()> thunk;
-        public:
-        raw_task_delay(std::function<A ()> a) : thunk(a) {}
+        template <class F>
+        auto flatMap(F func) const -> task<flatmap_t<F, T>> {
+            return {
+                [f = func, ta = *this]() {
+                    return f(ta.unsafeRunSync()).unsafeRunSync();
+                }
+            };
+        }
 
-        A unsafeRunSync() const {
+        T unsafeRunSync() const {
             return thunk();
         }
-
-        std::future<A> unsafeRunAsync() const {
+        std::future<T> unsafeRunAsync() const {
             return std::async(std::launch::async, thunk);
         }
     };
-
-    template<typename A, typename B>
-    class raw_task_flatmap final : public raw_task<B> {
-        task<A> ta;
-        std::function<task<B> (A)> f;
-        public:
-        raw_task_flatmap(task<A> a, std::function<task<B> (A)> b) : ta(a), f(b) {}
-
-        B unsafeRunSync() const {
-            A va = ta.unsafeRunSync();
-            task<B> tb = f(va);
-            B vb = tb.unsafeRunSync();
-            return vb;
-        }
-
-        std::future<B> unsafeRunAsync() const {
-            std::future<A> fa = ta.unsafeRunAsync();
-            std::future<B> fb = std::async(std::launch::async, [*this](std::future<A>&& fa) {
-                A a = fa.get();
-                task<B> t = f(a);
-                std::future<B> fb = t.unsafeRunAsync();
-                return fb.get();
-            }, std::move(fa));
-            return fb;
-        }
-    };
-
-    template<typename A, typename B>
-    class raw_task_map final : public raw_task<B> {
-        task<A> ta;
-        std::function<B (A)> f;
-        public:
-        raw_task_map(task<A> a, std::function<B (A)> b) : ta(a), f(b) {}
-
-        B unsafeRunSync() const {
-            A va = ta.unsafeRunSync();
-            B vb = f(va);
-            return vb;
-        }
-
-        std::future<B> unsafeRunAsync() const {
-            std::future<A> fa = ta.unsafeRunAsync();
-            std::future<B> fb = std::async(std::launch::async, [*this](std::future<A>&& fa) {
-                A a = fa.get();
-                return f(a);
-            }, std::move(fa));
-            return fb;
-        };
-    };
-
-    template<typename A>
-    class task {
-        std::shared_ptr<raw_task<A>> internal;
-
-        public:
-        task(std::shared_ptr<raw_task<A>> a) : internal(a) {}
-
-        static task<A> pure(A a) {
-            const auto inner = std::shared_ptr<raw_task<A>>(new raw_task_pure<A>(a));
-            return task<A>(inner);
-        }
-        static task<A> delay(std::function<A ()> thunk) {
-            const auto inner = std::shared_ptr<raw_task<A>>(new raw_task_delay<A>(thunk));
-            return task<A>(inner);
-        }
-
-        template<typename B>
-        task<B> flatMap(std::function<task<B> (A)> f) const {
-            const auto inner = std::shared_ptr<raw_task<B>>(new raw_task_flatmap<A, B>(*this, f));
-            return task<B>(inner);
-        }
-        template<typename B>
-        task<B> map(std::function<B (A)> f) const {
-            const auto inner = std::shared_ptr<raw_task<B>>(new raw_task_map<A, B>(*this, f));
-            return task<B>(inner);
-        }
-
-        A unsafeRunSync() const {
-            return internal->unsafeRunSync();
-        }
-
-        std::future<A> unsafeRunAsync() const {
-            return internal->unsafeRunAsync();
-        }
-    };
-
+    template <class F>
+    task(F func) -> task<decltype(func())()>;
 }
